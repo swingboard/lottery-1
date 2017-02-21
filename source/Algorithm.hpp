282e07d1c1c67d72f39559482c646c6425fe78c2
axilmar <axilmar@gmail.com>
2017-02-21 19:06:09 +0200

    wip

--- source/Algorithm.hpp
+++ source/Algorithm.hpp
@@ -25,6 +25,11 @@
 
 namespace lottery
 {
+
+
+    /**
+        Random number generator which uses c++17's random facilities.
+     */
     template <class T>
     class RandomNumberGenerator
     {
@@ -50,153 +55,6 @@ namespace lottery
         std::mt19937 m_randomNumberSeeder;
         std::uniform_int_distribution<> m_randomNumberDistribution;
     };
-
-
-    /**
-        Calculates the transitions and their probabilities from last number of values
-        using the given depth.
-     */
-    template <class T, class E>
-    void calculateTransitions(
-        const std::vector<T> &values,
-        const size_t depth,
-        const E epsilon,
-        std::vector<std::pair<T, double>> &transitions)
-    {
-        //map of values to frequencies
-        std::unordered_map<T, size_t> frequencies;
-
-        //current sequence begin iterator
-        const auto currentIt = values.end() - depth;
-
-        //total cases found
-        double total = 0;
-
-        //iterate all values
-        for (size_t valueIndex = 0; valueIndex < values.size() - 1 - depth; ++valueIndex)
-        {
-            //iterate all depth levels and compare each value of the two sequences
-            for (size_t depthIndex = 0; depthIndex < depth; ++depthIndex)
-            {
-                const T value1 = values[valueIndex + depthIndex];
-                const T value2 = *(currentIt + depthIndex);
-
-                //if sequences differ, find next sequence
-                if (std::abs(value1 - value2) > epsilon)
-                {
-                    goto NEXT_SEQUENCE;
-                }
-            }
-
-            //increase the sequence of the target value
-            const T targetValue = values[valueIndex + depth];
-            ++frequencies[targetValue];
-            ++total;
-
-            //next value
-        NEXT_SEQUENCE:
-            ;
-        }
-
-        //setup the transition table
-        for (const auto &p : frequencies) {
-            const double probability = p.second / total;
-            transitions.push_back(std::make_pair(p.first, probability));
-        }
-
-        //sort the transition table by probability in descending order
-        std::sort(
-            transitions.begin(),
-            transitions.end(),
-            TupleMemberComparator<std::greater<double>, 1>());
-    }
-
-
-    //calculate percentage.
-    inline double percent(double nom, double denom)
-    {
-        return nom / denom;
-    }
-
-
-    //calculate match percentage.
-    inline double matchPercent(double nom, double denom)
-    {
-        return 1.0 - percent(nom, denom);
-    }
-
-
-    /**
-        Returns the percentage of matching of a new transition value against previously known transitions.
-     */
-    template <class T>
-    double evaluateTransition(const T value, const std::vector<std::pair<T, double>> &transitions)
-    {
-        if (transitions.empty())
-        {
-            return 0;
-        }
-
-        double smallestDifference = std::numeric_limits<double>::max();
-        T closestValue;
-        T minValue = std::numeric_limits<T>::max();
-        T maxValue = std::min(std::numeric_limits<T>::min(), -std::numeric_limits<T>::max());
-
-        for (const auto &p : transitions)
-        {
-            const double delta = p.first - value;
-            const double absDelta = std::abs(delta);
-            if (absDelta < smallestDifference)
-            {
-                smallestDifference = absDelta;
-                closestValue = p.first;
-            }
-            minValue = std::min(minValue, p.first);
-            maxValue = std::max(maxValue, p.first);
-        }
-
-        const T maxDelta = maxValue != minValue ? maxValue - minValue : maxValue;
-        const T closestValueDelta = closestValue > value ? closestValue - value : value - closestValue;
-        const double pc = matchPercent(closestValueDelta, maxDelta);
-        return pc;
-    }
-
-
-    /**
-        Calculates the averages for each row for each column of the given data.
-        It also returns the final sums of the columns.
-     */
-    template <class T>
-    std::vector<std::vector<double>> calculateColumnAverages(
-        const std::vector<std::vector<T>> &data,
-        std::vector<double> &sums)
-    {
-        const size_t columnCount = data.size();
-        const size_t rowCount = data[0].size();
-
-        std::vector<std::vector<double>> results(columnCount, std::vector<double>(rowCount));
-
-        sums = std::vector<double>(columnCount);
-
-        for (size_t rowIndex = 0; rowIndex < rowCount; ++rowIndex)
-        {
-            for (size_t columnIndex = 0; columnIndex < columnCount; ++columnIndex)
-            {
-                const T value = data[columnIndex][rowIndex];
-                sums[columnIndex] += value;
-                results[columnIndex][rowIndex] = sums[columnIndex] / (rowIndex + 1);
-            }
-        }
-
-        return results;
-    }
-
-
-    template <class T>
-    T rnd(T min, T max)
-    {
-
-    }
 
 
     ///create a random number set.
--- source/SubGame.cpp
+++ source/SubGame.cpp
@@ -105,112 +105,39 @@ namespace lottery
             throw std::invalid_argument("minPredictedNumbersPerColumn shall not be zero");
         }
 
-        const size_t maxSuccess = 10;
-        std::vector<std::unordered_map<size_t, size_t>> totalSuccesses(maxSuccess);
-        std::vector<double> predictedNumberCounts(maxSuccess);
-        const size_t startResultRowIndex = getRowCount() - 100;
-        const size_t endResultRowIndex = getRowCount() - 1;
-        RandomNumberGenerator<Number> randomNumber(m_minNumber, m_maxNumber);
-        std::vector<std::unordered_map<size_t, size_t>> totalRandomSuccesses(maxSuccess);
+        //column sums
+        std::vector<int> columnSums(m_columnCount);
 
-        for (size_t resultIndex = startResultRowIndex; resultIndex < endResultRowIndex; ++resultIndex)
+        //column averages
+        std::vector<double> columnAverages(m_columnCount);
+
+        //row sums
+        std::vector<int> rowSums(getRowCount() - 1);
+
+        //row delta sums
+        std::vector<int> rowDeltaSums(getRowCount() - 1);
+
+        //iterate all rows except the last one, since the prediction is for the row next to the current one.
+        for (size_t rowIndex = 0; rowIndex < getRowCount() - 1; ++rowIndex)
         {
-            std::vector<std::vector<Number>> partialResults(m_columnCount);
+            //compute column sums and averages; 
+            //also compute sum of row
             for (size_t columnIndex = 0; columnIndex < m_columnCount; ++columnIndex)
             {
-                partialResults[columnIndex] = std::vector<Number>(m_results[columnIndex].begin(), m_results[columnIndex].begin() + resultIndex);
+                const Number number = m_results[columnIndex][rowIndex];
+                columnSums[columnIndex] += number;
+                columnAverages[columnIndex] = columnSums[columnIndex] / (double)(rowIndex + 1);
+                rowSums[rowIndex] += number;
             }
 
-            //calculate the number transitions
-            std::vector<std::vector<std::pair<Number, double>>> numberTransitions(m_columnCount);
-            for (size_t columnIndex = 0; columnIndex < m_columnCount; ++columnIndex)
+            //compute delta sums of row
+            for (size_t columnIndex = 0; columnIndex < m_columnCount - 1; ++columnIndex)
             {
-                calculateTransitions(partialResults[columnIndex], 1, 0, numberTransitions[columnIndex]);
-            }
-
-            //calculate the numbers' average transitions
-            std::vector<double> sums;
-            std::vector<std::vector<double>> columnAverages = calculateColumnAverages(partialResults, sums);
-            std::vector<std::vector<std::pair<double, double>>> columnAverageTransitions(m_columnCount);
-            for (size_t columnIndex = 0; columnIndex < m_columnCount; ++columnIndex)
-            {
-                calculateTransitions(columnAverages[columnIndex], 1, 0.001, columnAverageTransitions[columnIndex]);
-            }
-
-            //using the number transitions and average transitions, find the most probable transitions
-            const size_t maxRows = resultIndex + 1;
-            std::vector<std::vector<std::pair<Number, double>>> bestNumberTransitions(m_columnCount);
-            for (size_t columnIndex = 0; columnIndex < m_columnCount; ++columnIndex)
-            {
-                for (size_t numberIndex = 0; numberIndex < numberTransitions[columnIndex].size(); ++numberIndex)
-                {
-                    const Number number = numberTransitions[columnIndex][numberIndex].first;
-                    const double sum = sums[columnIndex] + number;
-                    const double average = sum / maxRows;
-                    const double match = evaluateTransition(average, columnAverageTransitions[columnIndex]);
-                    bestNumberTransitions[columnIndex].push_back(std::make_pair(number, match * numberTransitions[columnIndex][numberIndex].second));
-                }
-                std::sort(
-                    bestNumberTransitions[columnIndex].begin(),
-                    bestNumberTransitions[columnIndex].end(),
-                    TupleMemberComparator<std::greater<double>, 1>());
-            }
-
-            std::vector<size_t> successes(maxSuccess);
-            std::vector<size_t> randomSuccesses(maxSuccess);
-            std::unordered_set<Number> predictedNumbers;
-            for (size_t i = 0; i < maxSuccess; ++i)
-            {
-                for (size_t columnIndex = 0; columnIndex < m_columnCount; ++columnIndex)
-                {
-                    if (i < bestNumberTransitions[columnIndex].size())
-                    {
-                        predictedNumbers.insert(bestNumberTransitions[columnIndex][i].first);
-                    }
-                }
-
-                predictedNumberCounts[i] += predictedNumbers.size();
-
-                std::unordered_set<Number> randomNumbers = createRandomNumbers(randomNumber, predictedNumbers.size());
-
-                for (size_t columnIndex = 0; columnIndex < m_columnCount; ++columnIndex)
-                {
-                    if (predictedNumbers.find(m_results[columnIndex][resultIndex + 1]) != predictedNumbers.end())
-                    {
-                        ++successes[i];
-                    }
-
-                    if (randomNumbers.find(m_results[columnIndex][resultIndex + 1]) != randomNumbers.end())
-                    {
-                        ++randomSuccesses[i];
-                    }
-                }
-            }
-
-            for (size_t i = 0; i < maxSuccess; ++i)
-            {
-                ++totalSuccesses[i][successes[i]];
-                ++totalRandomSuccesses[i][randomSuccesses[i]];
+                const Number number1 = m_results[columnIndex][rowIndex];
+                const Number number2 = m_results[columnIndex + 1][rowIndex];
+                rowDeltaSums[rowIndex] += number2 - number1;
             }
         }
-
-        std::ofstream file("data.txt");
-        const size_t maxRows = endResultRowIndex - startResultRowIndex;
-        for (size_t i = 0; i < maxSuccess; ++i)
-        {
-            file << "Depth " << (i + 1) << " : \n";
-            for (size_t success = 1; success <= m_columnCount; ++success)
-            {
-                file << "    success of " << success << " : ";
-                file << std::setw(7) << std::fixed << std::setprecision(3) << (100.0*percent(totalSuccesses[i][success], maxRows)) << "% vs ";
-                file << std::setw(7)  << std::fixed << std::setprecision(3) << (100.0*percent(totalRandomSuccesses[i][success], maxRows)) << "%\n";
-            }
-            file << "    average count of predicted numbers per row = ";
-            const double predictedNumberCountPerRow = predictedNumberCounts[i] / maxRows;
-            file << std::fixed << std::setprecision(3) << predictedNumberCountPerRow << '\n';
-            file << '\n';
-        }
-        file << '\n';
 
         std::set<Number> predictedNumbers;
         return predictedNumbers;
