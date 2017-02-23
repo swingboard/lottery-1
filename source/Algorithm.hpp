9fc54b4ea2a422eb097ff766c680d20f3a92cdde
axilmar <axilmar@gmail.com>
2017-02-23 12:03:39 +0200

    wip

--- source/Algorithm.hpp
+++ source/Algorithm.hpp
@@ -30,37 +30,6 @@ namespace lottery
 {
 
 
-    ///returns min/max values.
-    template <class T>
-    std::pair<T, T> getMinMaxValues(const std::vector<T> &values)
-    {
-        std::pair<T, T> result(std::numeric_limits<T>::max(), -std::numeric_limits<T>::max());
-        for (T v : values)
-        {
-            result.first = std::min(result.first, v);
-            result.second = std::max(result.second, v);
-        }
-        return result;
-    }
-
-
-    //sum values
-    template <class RT, class T>
-    RT sum(const std::vector<T> &values)
-    {
-        return std::accumulate(values.begin(), values.end(), 0, [](RT a, RT b) { return a + b; });
-    }
-
-
-    //average of values
-    template <class RT, class T>
-    RT average(const std::vector<T> &values)
-    {
-        const RT s = sum<RT>(values);
-        return (RT)(s / (double)values.size());
-    }
-
-
 } //namespace lottery
 
 
--- source/SubGame.cpp
+++ source/SubGame.cpp
@@ -118,29 +118,6 @@ namespace lottery
             throw std::invalid_argument("minPredictedNumbersPerColumn shall not be zero");
         }
 
-        std::vector<std::unordered_map<Number, double>>
-            numberProbabilitiesByNextNumber = calculateProbabilitiesByNextValue(m_results);
-        std::vector<std::vector<std::pair<Number, double>>> 
-            sorted1 = sortProbabilities(numberProbabilitiesByNextNumber);
-
-        std::vector<std::unordered_map<Number, double>>
-            numberProbabilitiesByNextNumberDelta = calculateProbabilitiesByNextValueDelta(m_results);
-        std::vector<std::vector<std::pair<Number, double>>> 
-            sorted2 = sortProbabilities(numberProbabilitiesByNextNumberDelta);
-
-        std::vector<std::unordered_map<Number, double>>
-            numberProbabilitiesByNextNumberDeltaFromAverage = calculateProbabilitiesByNextValueDeltaFromAverage(m_results);
-        std::vector<std::vector<std::pair<Number, double>>> 
-            sorted3 = sortProbabilities(numberProbabilitiesByNextNumberDeltaFromAverage);
-
-        std::vector<std::unordered_map<Number, double>>
-            allProbabilities = multiplyProbabilities({
-                numberProbabilitiesByNextNumber,
-                numberProbabilitiesByNextNumberDelta,
-                numberProbabilitiesByNextNumberDeltaFromAverage});
-        std::vector<std::vector<std::pair<Number, double>>> sorted4 = 
-            sortProbabilities(allProbabilities);
-
         //TODO    
         std::set<Number> predictedNumbers;
         return predictedNumbers;
