5c4c7c2139e79cb3455acb89a2ac844421d2bd40
axilmar <axilmar@gmail.com>
2018-02-06 15:26:31 +0200

    Fixed comparison warning.

--- dev/Source/DynamicStackArray.hpp
+++ dev/Source/DynamicStackArray.hpp
@@ -7,7 +7,7 @@
  */
 #define LOTTERY_DYNAMIC_STACK_ARRAY(TYPE, NAME, SIZE, DEFAULT_VALUE)\
     TYPE *NAME = (TYPE *)alloca(sizeof(TYPE) * SIZE);\
-    for (size_t i = 0; i < SIZE; ++i) { NAME[i] = DEFAULT_VALUE; }
+    for (size_t i = 0; i < (size_t)SIZE; ++i) { NAME[i] = DEFAULT_VALUE; }
 
 
 #endif //LOTTERY_DYNAMIC_STACK_ARRAY_HPP
