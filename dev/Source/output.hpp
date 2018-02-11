b3f86454bfb70c03584b648afc3d2991ff4af1ad
axilmar <axilmar@gmail.com>
2018-02-11 02:22:26 +0200

    added output of draw as numbers instead of as characters.

--- dev/Source/output.hpp
+++ dev/Source/output.hpp
@@ -11,16 +11,16 @@ namespace Lottery
 {
 
 
-    template <class Stream, class Container> Stream &outputContainer(Stream &stream, const Container &container)
+    template <class T, class Stream, class Container> Stream &outputContainer(Stream &stream, const Container &container)
     {
         stream << '[';
         if (!container.empty())
         {
             auto it = container.begin();
-            stream << *it;
+            stream << (T)*it;
             for (++it; it != container.end(); ++it)
             {
-                stream << ", " << *it;
+                stream << ", " << (T)*it;
             }
         }
         stream << ']';
@@ -34,14 +34,21 @@ namespace Lottery
 template <class E, class Tr, class T, class Alloc> 
 std::basic_ostream<E, Tr> &operator << (std::basic_ostream<E, Tr> &stream, const std::vector<T, Alloc> &container)
 {
-    return Lottery::outputContainer(stream, container);
+    return Lottery::outputContainer<T>(stream, container);
+}
+
+
+template <class E, class Tr, class Alloc> 
+std::basic_ostream<E, Tr> &operator << (std::basic_ostream<E, Tr> &stream, const std::vector<uint8_t, Alloc> &container)
+{
+    return Lottery::outputContainer<size_t>(stream, container);
 }
 
 
 template <class E, class Tr, class T, class Less, class Alloc> 
 std::basic_ostream<E, Tr> &operator << (std::basic_ostream<E, Tr> &stream, const std::multiset<T, Less, Alloc> &container)
 {
-    return Lottery::outputContainer(stream, container);
+    return Lottery::outputContainer<T>(stream, container);
 }
 
 
