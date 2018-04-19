67c7bfcbac782efeb62094209a664c21078b6215
Achilleas Margaritis <extern.Achilleas.Margaritis@elektrobit.com>
2018-04-19 17:32:09 +0300

    Added dynamic stack array not initialized.

--- source/DynamicStackArray.hpp
+++ source/DynamicStackArray.hpp
@@ -10,4 +10,12 @@
     for (size_t i = 0; i < (size_t)SIZE; ++i) { NAME[i] = DEFAULT_VALUE; }
 
 
+/**
+    Declares a dynamic array on the stack, using alloca.
+    The array is not initialized.
+ */
+#define LOTTERY_DYNAMIC_STACK_ARRAY_NOT_INITIALIZED(TYPE, NAME, SIZE)\
+    TYPE *NAME = (TYPE *)alloca(sizeof(TYPE) * SIZE);
+
+
 #endif //LOTTERY_DYNAMIC_STACK_ARRAY_HPP
