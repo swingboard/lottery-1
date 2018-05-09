1a3c261eb138a1d1165e031ba73080389fd452db
Achilleas Margaritis <extern.Achilleas.Margaritis@elektrobit.com>
2018-05-09 16:33:44 +0300

    work in progress.

--- source/PredictionAlgorithmA.hpp
+++ source/PredictionAlgorithmA.hpp
@@ -14,29 +14,13 @@ namespace Lottery
     {
     public:
         PredictionAlgorithmA(const Game &game);
-
-        virtual std::string getName() const
-        {
-            return "A";
-        }
-
-        virtual void initialize(const SubGame &subGame, const DrawVectorRange &sampleDraws)
-        {
-            m_predictionAlgorithms[subGame.getIndex()]->initialize(subGame, sampleDraws);
-        }
-
-        virtual void predict(const SubGame &subGame, const DrawVectorRange &previousDraws, Prediction &prediction)
-        {
-            m_predictionAlgorithms[subGame.getIndex()]->predict(subGame, previousDraws, prediction);
-        }
-
-        virtual void finalize(const SubGame &subGame, const DrawVectorRange &sampleDraws)
-        {
-            m_predictionAlgorithms[subGame.getIndex()]->finalize(subGame, sampleDraws);
-        }
+        virtual std::string getName() const { return "A"; }
+        virtual void initialize(const SubGame &subGame, const DrawVectorRange &sampleDraws);
+        virtual void predict(const SubGame &subGame, const DrawVectorRange &previousDraws, Prediction &prediction);
+        virtual void finalize(const SubGame &subGame, const DrawVectorRange &sampleDraws);
 
     private:
-        std::vector<std::unique_ptr<PredictionAlgorithm>> m_predictionAlgorithms;
+        std::vector<std::vector<std::shared_ptr<PredictionAlgorithm>>> m_predictionAlgorithms;
     };
 
 
